9c7f83369b9a448f75a0afa83a01149ec23ed475
S. Buch <buc@hems.de>
2023-02-25 12:27:02 +0100

    kills all processes of word now

--- PDFTools.py
+++ PDFTools.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import psutil
 from tkinter import (BOTTOM, Button, Label, PhotoImage, Tk, Toplevel,
                      filedialog, messagebox)
 
@@ -17,7 +18,7 @@ def docx_to_pdf(docx_filename, pdf_filename):
         doc = word.Documents.Open(f'"{docx_filename}"')
 
     except comtypes.COMError as e:
-        messagebox.showerror(title = "Fehler", message = e)
+        messagebox.showerror(title = "Fehler", message = "Die Datei " + docx_filename + " kann nicht geöffnet werden.")
         word.Quit()
         return 0
 
@@ -27,7 +28,7 @@ def docx_to_pdf(docx_filename, pdf_filename):
         doc.SaveAs(pdf_filename, FileFormat=17)
       
     except comtypes.COMError as e:
-        messagebox.showerror(title = "Fehler", message = e)
+        messagebox.showerror(title = "Fehler", message = "Die Datei " + pdf_filename + " kann nicht angelegt werden.")
         doc.Close()
         word.Quit()
         return 0
@@ -45,7 +46,11 @@ def select_docx_files(convert_button):
     convert_button.config(state="disabled", text="...einen Moment bitte...")
     
     # close all instances of word to not create a mess...
-    messagebox.showwarning(title = "Bitte alle Word-Fenster schließen!", message = "Bitte schließen Sie alle Word-Fenster\nund klicken Sie dann auf OK.\nDies ist für einen sauberen Ablauf notwendig.")
+    messagebox.showwarning(title = "Word-Fenster schließen!", 
+                           message = "Schließen Sie alle Word-Fenster\nund klicken Sie dann auf OK.")
+ 
+    # kill all word instances (user has been warned)
+    killallword()
 
     # Open a file selection dialog and get the selected files.
     docx_filenames = filedialog.askopenfilenames(title='Word-Dateien zur Erzeugung von PDF auswählen', filetypes=[('Word Dokumente', '*.docx')]) 
@@ -67,9 +72,9 @@ def select_docx_files(convert_button):
         window.update()
     
     if pdfcount > 0:
-        show_temp_message('Erledigt', 'Es wurden insgesamt ' + str(pdfcount) + ' PDFs erzeugt.')
-    #enable the button again to create another batch of PDF files.
-    convert_button.config(state="active", text ="PDF aus DOCX erzeugen")
+        show_temp_message('Erledigt', 'Es wurde(n)\n' + str(pdfcount) + ' PDF erzeugt.')
+    
+    revert_button_text()
         
 def remove_metadata(meta_button):
     pdfcount = 0
@@ -128,25 +133,48 @@ def remove_metadata(meta_button):
             
         os.remove(temp_name)
             
-
-    # Reset button to original text
-    meta_button.config(state="active", text = "Metadaten aus PDF entfernen")
+    revert_button_text()
 
     if pdfcount >0:
-        show_temp_message("erledigt...", "Die Metadaten der " + str(pdfcount) + " PDFs wurden entfernt.")
+        show_temp_message("erledigt...", "Die Metadaten von\n" + str(pdfcount) + " PDF wurden entfernt.")
 
 def show_temp_message(title, message, seconds=5):
     # Create a new top-level window for the message.
     window = Toplevel()
+    window.overrideredirect(True)
+    #window.geometry("300x200")
     window.title(title)
    
+
     # Create a label for the message.
-    label = Label(window, text=message, font=("Helvetica", 12))
+    label = Label(window, text=message, font=("Helvetica", 50))
     label.pack()
     
     # Close the window after a certain number of seconds.
     window.after_idle(lambda: window.after(seconds * 1000, window.destroy))
 
+def revert_button_text():
+    # Reset button to original text
+    meta_button.config(state="active", text = "Metadaten aus PDF entfernen")
+    #enable the button again to create another batch of PDF files.
+    convert_button.config(state="active", text ="PDF aus DOCX erzeugen")
+
+def killallword():
+    # Iterate over all running processes
+    for proc in psutil.process_iter():
+        try:
+            # Get process details as a named tuple
+            process_info = proc.as_dict(attrs=['pid', 'name'])
+            process_name = process_info['name'].lower()
+        
+            # Check if the process is Microsoft Word
+            if 'winword.exe' == process_name:
+            # Terminate the process
+                process = psutil.Process(process_info['pid'])
+                process.terminate()
+                #time.sleep(2)
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
@@ -164,7 +192,7 @@ window = Tk()
 window.iconbitmap(resource_path("PDFTools.ico"))
 
 # Set the window title
-window.title("PDF-Tools v1.3 (buc @ hems.de)")
+window.title("PDF-Tools v1.4 (buc @ hems.de)")
 
 # Set the window size
 window.geometry("560x240")
@@ -178,11 +206,11 @@ label1.image = pimage
 label1.place(x=0, y= 0)
 
 # Add a button to start cleaning the PDFs
-meta_button = Button(text="Metadaten aus PDFs entfernen", command=lambda: remove_metadata(meta_button), font=("Helvetica", 14))
+meta_button = Button(text="Metadaten aus PDF entfernen", command=lambda: remove_metadata(meta_button), font=("Helvetica", 14))
 meta_button.pack(side=BOTTOM, pady=10)
 
 # Add a button to start converting the docx
-convert_button = Button(text ="PDFs aus DOCX erzeugen", command=lambda: select_docx_files(convert_button), font=("Helvetica", 14))
+convert_button = Button(text ="PDF aus DOCX erzeugen", command=lambda: select_docx_files(convert_button), font=("Helvetica", 14))
 convert_button.pack(side=BOTTOM)
 
 # Run the Tkinter event loop
